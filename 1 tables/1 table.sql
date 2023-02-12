935158318ed51b45e30ed7c2963b89e0f5f65af1
puahisrael <puahisrael1@gmail.com>
2023-02-12 15:48:07 -0600

    made the num column nullable

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,5 +1,7 @@
 use humanbodyDB
 go 
+drop table if exists body 
+go
 create table dbo.body(id int not null identity primary key, 
 bodypart varchar(100) not null, 
-num int not null)
+num int null)
