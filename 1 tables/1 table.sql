0b326fdea5f9be09657e6f2cc0f3fe0e89b907f4
puahisrael <puahisrael1@gmail.com>
2023-02-12 15:52:41 -0600

    first commit for the trial branch

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,5 +1,9 @@
 use humanbodyDB
 go 
-create table dbo.body(id int not null identity primary key, 
+drop table if exists body
+go 
+create table dbo.body(
+id int not null identity primary key, 
+creaturetype varchar(25) not null,
 bodypart varchar(100) not null, 
 num int not null)
